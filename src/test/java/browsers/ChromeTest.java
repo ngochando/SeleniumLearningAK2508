e8bfd47978c7a5c014692bdb579b56453e0bfcac
ngochanday <hanvan1331@gmail.com>
2025-08-09 16:03:10 +0700

    Mobile chrome mode

--- src/test/java/browsers/ChromeTest.java
+++ src/test/java/browsers/ChromeTest.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChromeTest {
 
 //    @Test
@@ -29,4 +32,22 @@ public class ChromeTest {
         Assert.assertEquals(driver.getTitle(),"Selenium");
         driver.quit();
     }
+
+    @Test
+    void openBrowserWithMobileViewMode(){
+        Map<String, Object> deviceMetrics = new HashMap<>();
+        deviceMetrics.put("width", 344);
+        deviceMetrics.put("height", 882);
+        Map<String, Object> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceMetrics", deviceMetrics);
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+
 }
