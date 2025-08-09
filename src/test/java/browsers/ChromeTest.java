9080a54bc8bcc0fd94e84e64807eef46deb8bc7c
ngochanday <hanvan1331@gmail.com>
2025-08-09 16:32:09 +0700

    final day 1

--- src/test/java/browsers/ChromeTest.java
+++ src/test/java/browsers/ChromeTest.java
@@ -3,24 +3,29 @@ package browsers;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.v125.network.Network;
+import org.openqa.selenium.devtools.v125.network.model.ConnectionType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class ChromeTest {
 
-//    @Test
-//    public void normalModeTest(){
-//        WebDriver driver = new ChromeDriver();
-//
-//        driver.get("https://www.selenium.dev/");
-//
-//        Assert.assertEquals(driver.getCurrentUrl(), "https://www.selenium.dev/");
-//
-//        driver.quit();
-//    }
+    @Test
+    public void normalModeTest(){
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.selenium.dev/");
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.selenium.dev/");
+
+        driver.quit();
+    }
 
     @Test
     public void headlessModeTest(){
@@ -50,4 +55,36 @@ public class ChromeTest {
         driver.quit();
     }
 
+    @Test
+    void openBrowserWithBetaVersion(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("132");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+
+    @Test
+    void simulate3GNetworkCondition(){
+        ChromeDriver driver = new ChromeDriver();
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+        // Enable Network emulation
+        devTools.send(Network.enable(Optional.of(100000000), Optional.empty(), Optional.empty()));
+
+        // Set network conditions to emulate 3G or 4G
+        devTools.send(Network.emulateNetworkConditions(
+                false,
+                100,
+                75000,
+                25000,
+                Optional.of(ConnectionType.CELLULAR2G),
+                Optional.of(0),
+                Optional.of(0),
+                Optional.of(false)
+        ));
+        driver.get("https://selenium.dev");
+    }
 }
